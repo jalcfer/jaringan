533542dede4f6cc40f4fc5bc24e3c18a533c5f1c
jalcfer <jalcfer@gmail.com>
2018-08-01 13:55:25 -0500

    firs commit for jaringan

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+let app;
+let config = {
+  apiKey: "AIzaSyDVsLk6bMpkYbgyL0g7Y2DSt9F4UCTWQ_o",
+  authDomain: "jaringan-cbc53.firebaseapp.com",
+  databaseURL: "https://jaringan-cbc53.firebaseio.com",
+  projectId: "jaringan-cbc53",
+  storageBucket: "jaringan-cbc53.appspot.com",
+  messagingSenderId: "176595528663"
+};
+firebase.initializeApp(config);
+firebase.auth().onAuthStateChanged((user)=>{
+  if(!app){
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      router,
+      components: { App },
+      template: '<App/>'
+    })
+  }
+})
